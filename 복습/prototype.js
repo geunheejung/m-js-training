061bface8c9af0bd46cdd3e71265dbb2975baa21
정근희 <geuni1013@gmail.com>
2018-05-22 14:37:19 +0900

    class 공부중 prototype 및 상속 복습

---
+++ 복습/prototype.js
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////
+// MDN Introduction to Object-Oriented JavaScript //
+////////////////////////////////////////////////////
+// Person
+function Person (firstName) {
+  this.firstName = firstName;
+}
+
+Person.prototype.walk = function(){
+  console.log("I am walking!");
+};
+
+Person.prototype.sayHello = function(){
+  console.log("Hello, I'm " + this.firstName);
+};
+
+// Student
+function Student(firstName, subject) {
+  Person.call(this, firstName);
+
+  this.subject = subject;
+}
+
+// Student.prototype
+Student.prototype = Object.create(Person.prototype);
+// Student.prototype.constructor = Student;
+
+Student.prototype.sayHello = function(){
+  console.log("Hello, I'm " + this.firstName + ". I'm studying "
+    + this.subject + ".");
+};
+
+Student.prototype.sayGoodBye = function(){
+  console.log("Goodbye!");
+};
+
+var student = new Student('geuni', 'math');
+
+student.walk();
+
